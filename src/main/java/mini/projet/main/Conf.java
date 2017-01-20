473548e42f3b93d9111db5492f04452eca8185f5
kouki monem <abdalmonem.kouki@ensi-uma.tn>
2017-01-20 16:18:04 +0100

    First commit

---
+++ src/main/java/mini/projet/main/Conf.java
@@ -0,0 +1,16 @@
+package mini.projet.main;
+
+public final class Conf {
+   public static final String TOPICNAME = "koukiProj";
+   public static final String CONSUMERURI = "kouki:8081";
+   public static final String PRODUCERURI = "kouki:8081";
+   public static final String GROUP = "kouki-miniprojet-group-1";
+   public static final String ADRESS_ES = "127.0.0.1";
+   public static final int PORT_ES = 9300;
+   public static final String REGEX_PATTERN_FACTUR = "(.*);(.*);(.*);(.*);(.*);(.*);(.*);(.*)";
+   public static final String REGEX_PATTERN_FACTUR_SEOND = "(.*)|(.*)|(.*)|(.*)|(.*)|(.*)|(.*)|(.*)|(.*)|(.*)|(.*)";
+	
+	private Conf() {
+
+	}
+}
